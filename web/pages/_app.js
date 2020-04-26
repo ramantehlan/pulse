5338464c1d72cf2265d70f31dcfbe7b51cb2c4df
raman tehlan <ramantehlan@gmail.com>
2020-04-27 01:49:17 +0530

    - Major changed | First draft of ui - Remove typescript (To make development simple for me) - add next.config - add styled

---
+++ web/pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/global.css'
+
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
+
